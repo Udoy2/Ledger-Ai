a0d778a9065c1f94674fb08592ac9b997bd2e848
Ibnul Mohammad Adib <adib.bdhk@gmail.com>
2026-05-28 09:34:40 +0600

    pitch deck

---
+++ app/pitch/page.tsx
@@ -0,0 +1,10 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+export default async function PitchPage() {
+  const filePath = path.join(process.cwd(), 'docs', 'PulseDesk_Hackathon_Pitch.html');
+  const html = await readFile(filePath, 'utf-8');
+
+  return <div dangerouslySetInnerHTML={{ __html: html }} />;
+}
+
